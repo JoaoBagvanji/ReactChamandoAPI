df3121786f7fbc2c160d037ca75b1a2ab12afad5
João Bagvanji Junior <77748663+JoaoBagvanji@users.noreply.github.com>
2022-02-06 21:59:36 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
-import React, { Component } from 'react';
+import React from "react";
+import {  StyleSheet, View, Text } from 'react-native';
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state ={
-      posts:[]
-    }
-  }
-  //usando metodo fetch para chamar API no react, uma JSON Placeholder para teste
-  componentDidMount(){
-    const url = "https://jsonplaceholder.typicode.com/albums/1/photos";
-    fetch (url)
-    .then(response => response.json())
-    .then(json => this.setState({ posts: json }))
-  }
-  render(){
-    const { posts } =this.state;
-    return(
-      <div className='container'>
-        <div class="jumbotron">
-          <h1 class="display-4">Posts from our API call</h1>
-        </div>
-        {posts.map((post) =>(
-          <div className='card' key={post.id}>
-            <div className='card-header'>
-              ID #{post.id} {post.title}
-            </div>
-            <div className='card-body'>
-              <img src={post.thumbnailUrl}></img>
-            </div>
-          </div>
-        ))}
-      </div>
-    );
-  }
+export default function App(){
+  return(
+    <View style={styles.container}>
+      <Text> Minha primeira App React Native</Text>
+      <Text style={styles.titulo}>Ola Mundo!</Text>
+      <Text style={styles.descricao}>Aprendendo do Zero ao Pro React Native</Text>
+    </View>
+  );
 }
-export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo: {
+    fontSize: 20,
+    color: '#ff0021',
+  },
+  descricao: {
+    color: 'blue',
+    fontSize: 16,
+  }
+});
